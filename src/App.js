d2cbbdb830b4245ca3cad30944371efb5622f6a6
eeazzy <khs2498@gmail.com>
2024-01-16 14:30:59 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import R003_ImportConponent from './R003_ImportComponent';
 import R004_LifecycleEx from './R004_LifecycleEx';
 import R005_LifeCycleEx from './R005_LifecycleEx';
+import R006_LifecycleEx from './R006_LifecycleEx';
+import R007_LifecycleEx from './R007_LifecycleEx';
+import R008_LifecycleEx from './R008_LifecycleEx';
+import R009_Es6 from './R009_Es6';
 
 function App() {
   return (
-    <R005_LifeCycleEx></R005_LifeCycleEx>
+    <div>
+      <Es6/>
+    </div>
   );
 }
 
